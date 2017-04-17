b0aefa2e01dc4ad7c219452499ddb1417a1691e7
Kelli Parris <kelli@hsdesignhouse.com>
2017-04-17 15:14:12 -0500

    killing time before I have menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'enrG+', to: 'static_pages#enrG+', as: 'enrg-plus'
+  get 'menu' => 'static_pages#menu'
+
+
+  get 'home2' => 'static_pages#home2'
   get 'page' => 'static_pages#page'
+
 
   get '/fb', to: 'links#fb'
   get '/insta', to: 'links#insta'
