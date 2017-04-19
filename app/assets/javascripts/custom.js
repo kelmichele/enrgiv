2dfadcdc306a5a2cc62ab6bb45874c28547e8ec3
Kelli Parris <kelli@hsdesignhouse.com>
2017-04-19 16:57:26 -0500

    added bottom section of plus page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -25,4 +25,15 @@ $(document).ready(function() {
         gapVertical: 0,
     });
 
+    $('#acc2').cubeportfolio({
+        filters: '#filters-faq',
+        defaultFilter: '*',
+        animationType: 'sequentially',
+        gridAdjustment: 'default',
+        displayType: 'default',
+        caption: 'expand',
+        gapHorizontal: 0,
+        gapVertical: 0,
+    });
+
 }); // End of document.ready
