33e7e8a9307b2d36456fd5045e3f5c7849c9e43a
Kelli Parris <kelli@hsdesignhouse.com>
2017-05-18 16:24:25 -0500

    home page changes

--- app/assets/javascripts/slick-init.js
+++ app/assets/javascripts/slick-init.js
@@ -6,30 +6,5 @@ $(document).ready(function(){
     	slidesToScroll: 1,
     	autoplay: true,
     	autoplaySpeed: 3000,
-	  	responsive: [
-	  		{
-	      		breakpoint: 768,
-	      		settings: {
-			    	autoplaySpeed: 2500,
-		      	}
-		    },
-		    {
-		      	breakpoint: 640,
-	      		settings: {
-			    	dots: false,
-	      			slidesToShow: 4,
-			    	autoplaySpeed: 2500,
-	      		}
-	      	},
-	      	{
-		      	breakpoint: 320,
-	      		settings: {
-			    	dots: false,
-	      			slidesToShow: 3,
-			    	autoplaySpeed: 2500,
-	      		}
-	      	}
-	    ]
-
   	});
 });
